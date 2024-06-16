8d68cf0ce25f1f16529c6687d2a1ea7954e646aa
Raziq Rakee <97151754+raziqrakee@users.noreply.github.com>
2024-06-16 23:57:52 +0800

    update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,10 +32,6 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
@@ -45,13 +41,8 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: process.env.API_BASE_URL || 'http://localhost:8000',
   },
-
-  buildModules: [
-    '@nuxtjs/dotenv',
-  ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
